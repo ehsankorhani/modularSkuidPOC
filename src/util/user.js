293a7f862fe431a61bac353929f2655b8abfa963
Ehsan Korhani <ehsan.korhani@outlook.com>
2018-12-28 10:04:20 +1100

    getemail

--- src/util/user.js
+++ src/util/user.js
@@ -1,10 +1,7 @@
 
-getUserEmail = (teamMember) => {
+export const getUserEmail = (teamMember) => {
   if (teamMember && teamMember.Email)
     return teamMember.Email;
 
   return 'Anonymous';
 }
-
-//export {getUserEmail as UserEmail};
-export {getUserEmail};
